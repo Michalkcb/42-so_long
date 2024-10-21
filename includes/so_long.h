d68e02a93ba4406aa1cf1f8066b8a47ea279fad5
Michael <michalkcb@gmail.com>
2024-10-21 21:41:18 +0200

    add includes

---
+++ includes/so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michalkcb <michalkcb@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 20:50:51 by michalkcb         #+#    #+#             */
+/*   Updated: 2024/10/21 21:35:42 by michalkcb        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+#include "../libft/libft.h"
+#include "../minilibx-linux/mlx.h"
+#include "../minilibx-linux/mlx_int.h"
+#include "get_next_line.h"
+#include "../ft_printf/ft_printf.h"
+
+
+
+#endif
