a6e65ea771b3382383dea9d2a94cc8f177b5fe02
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-10-27 16:40:27 +0100

    add

--- srcs/messages.c
+++ srcs/messages.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 18:11:24 by mbany             #+#    #+#             */
-/*   Updated: 2024/10/24 18:11:45 by mbany            ###   ########.fr       */
+/*   Updated: 2024/10/27 16:35:24 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,26 @@ void	exit_message(t_game *game, char *message, int code)
 
 	if (message)
 	{
-		ft_printf("%s\n", message);
+		if (ft_strcmp(message, "Loose") == 0)
+			ft_printf("You Loose\n");
+		else if (ft_strcmp(message, "Win") == 0)
+		{
+			ft_printf("You finished the game in %d moves!\n", game->moves);
+			fd = open("victory.txt", O_RDONLY);
+			line = get_next_line(fd);
+			while (line)
+			{
+				ft_printf("%s", line);
+				free(line);
+				line = get_next_line(fd);
+			}
+			close(fd);
+		}
+		else
+			ft_printf("Map is incorrect or you aborted game after %d moves \n",
+				game->moves);
 		exit(code);
 	}
-	ft_printf("You finished the game in %d moves!\n", game->moves);
-	fd = open("victory.txt", O_RDONLY);
-	line = get_next_line(fd);
-	while (line)
-	{
-		ft_printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	ft_printf("\n");
-	close(fd);
-	exit(code);
 }
 
 void	destroy_images(t_game *game)
